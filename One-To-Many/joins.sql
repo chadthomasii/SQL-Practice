56322c780394db04abfef61318e954a9a7f5ac3a
Chad Thomas <chad.thomas.ii@gmail.com>
2017-11-24 18:51:32 -0500

    Completed working on one-to-many relationship

--- One-To-Many/joins.sql
+++ One-To-Many/joins.sql
@@ -8,7 +8,7 @@ FROM customers, orders
 
 SELECT first_name, last_name, order_date, amount 
 FROM customers
-JOIN orders
+LEFT JOIN orders
     ON customers.id = orders.customer_id;
 
 
@@ -22,8 +22,37 @@ GROUP BY orders.customer_id
 ORDER BY TotalSpent;
 
 -- LEFT JOIN
-SELECT first_name, last_name, order_date, amount 
+SELECT first_name, last_name, IFNULL(order_date, 'N/A') AS order_date, IFNULL(SUM(amount), 0) AS total_spent
 FROM customers
 LEFT JOIN orders
     ON customers.id = orders.customer_id
-ORDER BY orders.amount;
+GROUP BY customers.id 
+ORDER BY total_spent;
+
+-- RIGHT JOIN
+SELECT *
+FROM customers
+RIGHT JOIN orders
+    ON customers.id = orders.customer_id
+GROUP BY customers.id 
+ORDER BY total_spent;
+
+-- ON DELETE CASCADE
+
+CREATE TABLE customers
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+CREATE TABLE orders
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT, 
+    FOREIGN KEY(customer_id) REFERENCES customers(id)
+    ON DELETE CASCADE 
+);
